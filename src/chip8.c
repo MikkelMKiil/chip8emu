5c1fd927c85135b4991b4d42ff9b658078a3e674
MikkelMKiil <mikkeltom@hotmail.dk>
2026-05-24 19:46:13 +0200

    Refactor chip8_load_rom to remove unnecessary size check and streamline file reading

--- src/chip8.c
+++ src/chip8.c
@@ -31,12 +31,10 @@ void chip8_init(Chip8 *c) {
 }
 int chip8_load_rom(Chip8 *c, const char *path){
     FILE *f = fopen(path, "rb");
+    fseek(f, 0, SEEK_END);
     long size = ftell(f);
-    if(size > (MEMORY_SIZE - PROGRAM_START)){
-        fclose(f);
-        return 1;
-    }
-    size_t read = fread(&c->memory[PROGRAM_START], 1, (size_t)size, f);
+    fseek(f, 0, SEEK_SET);
+    fread(&c->memory[PROGRAM_START], 1, (size_t)size, f);
     fclose(f);
     return 0;
 }
