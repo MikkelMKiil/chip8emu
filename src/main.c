f0ff3be79bb0f48263d442572de09834af2268b6
MikkelMKiil <mikkeltom@hotmail.dk>
2026-05-24 19:25:03 +0200

    Implement main loop for Chip8 emulator to continuously emulate cycles after loading ROM

--- src/main.c
+++ src/main.c
@@ -2,11 +2,17 @@
 
 
 int main(int argc, char *argv[]){
+    int run = 1;
     if(argc < 2){
         return 2;
     }
     else{
         Chip8 chip;
         chip8_init(&chip);
+        chip8_load_rom(&chip, argv[1]);
+        while(run == 1){
+            chip8_emulate_cycle(&chip);
+            
+        }
     }
 }
